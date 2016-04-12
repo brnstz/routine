fa3e7a04ae6af217c7387f315421efb73f6c24e4
Brian Seitz <brnstz@gmail.com>
2016-04-11 23:48:41 -0400

    making it a webserver instead of bothering with terminal colors

--- 01.go
+++ 01.go
@@ -2,62 +2,87 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
+	"image"
+	"image/color"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
 	"net/http"
 
 	"github.com/brnstz/routine/wikimg"
 )
 
 func main() {
-	// Create a temporary dir to store our images
-	dir, err := ioutil.TempDir("", "")
-	if err != nil {
-		panic(err)
-	}
 
-	fmt.Println("images will be saved to: ", dir)
+	http.HandleFunc("/colors", getImages)
 
-	p := wikimg.NewPuller(10000)
+	http.ListenAndServe(":8000", nil)
+}
 
-	i := 0
+func getImages(w http.ResponseWriter, r *http.Request) {
+	p := wikimg.NewPuller(1000)
+
 	for {
 		imgURL, err := p.Next()
 		if err == wikimg.EndofResults {
 			break
 
 		} else if err != nil {
-			panic(err)
+			log.Println(err)
+			continue
 		}
 
-		fmt.Println(imgURL, i)
-		i++
-	}
+		go func() {
+			c, err := getAvgColor(imgURL)
+			if err != nil {
+				log.Println(err)
+				return
+			}
 
+			fmt.Fprintf(w, `<div style="background: #%x%x%x; width=100%%">#%x%x%x</div>`, c.R, c.G, c.B, c.R, c.G, c.B)
+
+			f, ok := w.(http.Flusher)
+			if ok {
+				f.Flush()
+			}
+		}()
+	}
 }
 
-// getImage downloads the contents of imageURL to file in dir
-func getImage(dir, imageURL string) error {
-	// create a temporary file for saving the image
-	file, err := ioutil.TempFile(dir, "")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+func getAvgColor(imgURL string) (avgColor color.RGBA, err error) {
+	var r, g, b, a uint32
+	var pixels, tR, tG, tB, tA uint64
 
 	// call the image server
-	resp, err := http.Get(imageURL)
+	resp, err := http.Get(imgURL)
 	if err != nil {
-		return err
+		return
 	}
 	defer resp.Body.Close()
 
-	// copy from the HTTP response to our temporary file
-	_, err = io.Copy(file, resp.Body)
+	img, _, err := image.Decode(resp.Body)
 	if err != nil {
-		return err
+		return
 	}
 
-	// success!
-	return nil
+	rect := img.Bounds()
+	for x := 0; x < rect.Dx(); x++ {
+		for y := 0; y < rect.Dy(); y++ {
+			pixels++
+			r, g, b, a = img.At(x, y).RGBA()
+			tR += uint64(r)
+			tG += uint64(g)
+			tB += uint64(b)
+			tA += uint64(a)
+		}
+	}
+	avgColor = color.RGBA{
+		uint8(tR / pixels),
+		uint8(tG / pixels),
+		uint8(tB / pixels),
+		uint8(tA / pixels),
+	}
+
+	return
 }
